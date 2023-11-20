0cc308dcff884ec2a020de37169563b4379d4be3
qka10777 <qka10777@gamil.com>
2023-11-20 09:20:52 +0900

    reservation1

--- src/main/java/com/project/Restaurant/Reservation/Reservation.java
+++ src/main/java/com/project/Restaurant/Reservation/Reservation.java
@@ -1,33 +1,31 @@
-package com.project.Restaurant.Reservation;
+  package com.project.Restaurant.Reservation;
 
-import com.project.Restaurant.Member.Member;
-import com.project.Restaurant.Place.Place;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+  import com.project.Restaurant.Member.Member;
+  import com.project.Restaurant.Place.Place;
+  import jakarta.persistence.*;
+  import lombok.Getter;
+  import lombok.Setter;
 
-import java.time.LocalDateTime;
+  import java.time.LocalDateTime;
 
-@Entity
-@Getter
-@Setter
-public class Reservation {
+  @Entity
+  @Getter
+  @Setter
+  public class Reservation {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  private String storeId;
+    private String storeId;
+    private String ownerId;
+    private String customerId;
+    private LocalDateTime reservationTime;
+    private boolean confirmed; // 예약이 확인된 상태인지 여부
 
-  private String ownerId;
-
-  private String customerId;
-
-  private LocalDateTime localDateTime;
-
-//  @ManyToOne
-//  private Member member;
-//
-//  @ManyToOne
-//  private Place place;
-}
+  //  @ManyToOne
+  //  private Member member;
+  //
+  //  @ManyToOne
+  //  private Place place;
+  }
