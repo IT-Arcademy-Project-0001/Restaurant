815cae0d56595d052fc341d1e2b79948f41d122e
thisisnine99 <138759977+thisisnine99@users.noreply.github.com>
2023-11-20 09:13:38 +0900

    Login signup (#7)
    * 유저 권한이 마이페이지에 나오게 템플릿추가함
    
    * 개인회원가입버튼하고 사업자회원가입버튼 따로들어가도록
    
    ---------
    
    Co-authored-by: thisisnine99 <dkdlel2099@gmail.con>

--- src/main/java/com/project/Restaurant/Member/MemberController.java
+++ src/main/java/com/project/Restaurant/Member/MemberController.java
@@ -1,9 +1,11 @@
+
 package com.project.Restaurant.Member;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -12,6 +14,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -56,12 +61,42 @@ public class MemberController {
         return "redirect:/member/login";
     }
 
+    @PostMapping("/signupSeller")
+    public String signupSeller(@Valid MemberCreateForm memberCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        if (!memberCreateForm.getPassword().equals(memberCreateForm.getPasswordConfirm())) {
+            bindingResult.rejectValue("passwordConfirm", "passwordInCorrect",
+                    "패스워드가 일치하지 않습니다.");
+            return "signup_form";
+        }
+        try {
+            memberService.createSeller(memberCreateForm.getUsername(), memberCreateForm.getEmail(),
+                    memberCreateForm.getPassword(), memberCreateForm.getMemberNickName());
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
+        return "redirect:/member/login";
+    }
+
     @GetMapping("/information")
     public String memberInformation(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         Member targetMember = memberService.getMemberByUsername(userDetails.getUsername());
+
         model.addAttribute("targetMember", targetMember);
+        model.addAttribute("memberAuthorities", userDetails.getAuthorities());
+
+        System.out.println(userDetails.getAuthorities());
         return "memberInformation";
     }
 }
