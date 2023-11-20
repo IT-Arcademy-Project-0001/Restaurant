9254830c53c8c9e761d3a7bed9800ffdee590e98
mercurios0603 <67107715+mercurios0603@users.noreply.github.com>
2023-11-20 16:55:46 +0900

    placesearch (#9)

---
+++ src/main/java/com/project/Restaurant/DataNotFoundException.java
@@ -0,0 +1,16 @@
+package com.project.Restaurant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "entity not found")
+public class DataNotFoundException extends RuntimeException {
+
+
+  // 직렬화와 연관, 1은 int 데이터형 정수 리터럴, 1L은 long 데이터형 long 정수 리터럴
+  private static final long serialVersionUID = 1L;
+  public DataNotFoundException(String message) {
+    super(message);
+  }
+
+}
---
+++ src/main/java/com/project/Restaurant/Place/PlaceRepository.java
@@ -0,0 +1,7 @@
+package com.project.Restaurant.Place;
+
+import com.project.Restaurant.Member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+}
