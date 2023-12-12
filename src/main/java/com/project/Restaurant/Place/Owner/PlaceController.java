62f4d1e8d4a07d9454acfbc1a38e30d79150f6ea
jang48 <140681707+jang48@users.noreply.github.com>
2023-12-12 17:24:46 +0900

    Final (#69)
    * 거의다된듯하지만 안된
    
    * 지움
    
    * 그만
    
    * 찐 마지막
    
    * 틀린거확인

--- src/main/java/com/project/Restaurant/Place/Owner/PlaceController.java
+++ src/main/java/com/project/Restaurant/Place/Owner/PlaceController.java
@@ -96,20 +96,13 @@ public class PlaceController {
 //    return "Place/PlaceRegistList";
 //  }
 
-  @GetMapping("regist/list")
-<<<<<<< HEAD
-  public String list(Principal principal, Model model, @RequestParam(name = "parameter", required = false) String parameter,@RequestParam(value="page", defaultValue="0") int page,
-=======
-  public String list(Principal principal, Model model, @RequestParam(value="page", defaultValue="0") int page,
->>>>>>> c1b1d2c005033cc779f5848e30232c9c7536773b
+  @GetMapping("regist/list") 
+  public String list(Principal principal, Model model, @RequestParam(name = "parameter", required = false) String parameter,@RequestParam(value="page", defaultValue="0") int page, 
                      @RequestParam(value = "kw", defaultValue = "") String kw){
     Owner owner = this.ownerService.findByusername(principal.getName());
     Page<PlaceOwner> paging = this.placeService.getList(page,owner.getId(),kw);
-    model.addAttribute("paging",paging);
-<<<<<<< HEAD
-    model.addAttribute("parameter", parameter);
-=======
->>>>>>> c1b1d2c005033cc779f5848e30232c9c7536773b
+    model.addAttribute("paging",paging); 
+    model.addAttribute("parameter", parameter);  
     return "Place/PlaceRegistList";
   }
   @GetMapping("regist/list/detail/{id}")
