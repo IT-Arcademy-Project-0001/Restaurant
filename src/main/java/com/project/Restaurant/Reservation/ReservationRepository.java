98097ebd1d6c68aa54ae7f7009b3e265859c9000
qka10777 <qka10777@gmail.com>
2023-12-13 17:36:52 +0900

    reservation

--- src/main/java/com/project/Restaurant/Reservation/ReservationRepository.java
+++ src/main/java/com/project/Restaurant/Reservation/ReservationRepository.java
@@ -3,6 +3,7 @@ package com.project.Restaurant.Reservation;
 import com.project.Restaurant.Member.owner.Owner;
 import com.project.Restaurant.Place.Owner.PlaceOwner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +15,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
 
     List<Reservation> findByPlaceOwner_Id(Long id);
+
 
 }
 
